46dead85b6eff04703b72e3a4e4574cc58db7607
jane7166 <122887267+jane7166@users.noreply.github.com>
2025-03-03 20:17:28 +0900

    Update auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -14,16 +14,16 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
-    async session({ session, token }) {
-      session.accessToken = token.accessToken;
-      session.user.id = token.sub || "";
-      return session;
-    },
     async jwt({ token, account }) {
       if (account && account.access_token) {
         token.accessToken = account.access_token;
       }
       return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      session.user.id = token.sub || "";
+      return session;
     },
   },
 };
