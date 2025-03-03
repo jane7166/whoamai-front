bbb588363cc0bd870a665ef81ae7a559699ed1b6
Hyunlee <105576592+hyunleee@users.noreply.github.com>
2025-03-03 17:13:00 +0900

    배포 오류 수정 중

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
   eslint: {
     ignoreDuringBuilds: true,
   },
